b96f96a6beeef6603720eb7d80a14e25271d34d1
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-21 19:49:30 +0530

    Added: Code modified according to DRY principle

--- src/main/java/quantitymeasurement/Length.java
+++ src/main/java/quantitymeasurement/Length.java
@@ -1,12 +1,17 @@
 package quantitymeasurement;
 
-import java.util.Objects;
-
 public class Length {
-    enum Unit {FEET,INCH};
+    private static final Double FEET_TO_INCH = 12.0,YARD_TO_FEET=3.0,YARD_TO_INCH=36.0;
+
+    enum Unit {FEET, INCH, YARD}
 
     private final double value;
     private final Unit unit;
+
+    public Length(Unit unit, double value) {
+        this.value = value;
+        this.unit = unit;
+    }
 
     public Unit getUnit() {
         return unit;
@@ -16,16 +21,42 @@ public class Length {
         return value;
     }
 
-    public Length(Unit unit, double value) {
-        this.value = value;
-        this.unit = unit;
+    public boolean compare(Length that) {
+        if(that == null){
+            return false;
+        }
+        if(this.getUnit().equals(that.getUnit())){
+            return this.equals(that);
+        }
+        if(this.getUnit().equals(Unit.FEET) && that.getUnit().equals(Unit.INCH)){
+            return Double.compare(this.getValue()*FEET_TO_INCH,that.getValue())==0;
+        }
+        if(this.getUnit().equals(Unit.INCH) && that.getUnit().equals(Unit.FEET)){
+            return Double.compare(this.getValue()/FEET_TO_INCH,that.getValue())==0;
+        }
+        if(this.getUnit().equals(Unit.FEET) && that.getUnit().equals(Unit.YARD)){
+            return Double.compare(this.getValue()/YARD_TO_FEET,that.getValue())==0;
+        }
+        if(this.getUnit().equals(Unit.YARD) && that.getUnit().equals(Unit.FEET)){
+            return Double.compare(this.getValue()*YARD_TO_FEET,that.getValue())==0;
+        }
+        if(this.getUnit().equals(Unit.INCH) && that.getUnit().equals(Unit.YARD)){
+            return Double.compare(this.getValue()/YARD_TO_INCH,that.getValue())==0;
+        }
+        if(this.getUnit().equals(Unit.YARD) && that.getUnit().equals(Unit.INCH)){
+            return Double.compare(this.getValue()*YARD_TO_INCH,that.getValue())==0;
+        }
+        return false;
     }
 
+
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Length length = (Length) o;
+    public boolean equals(Object that) {
+        if (this == that) return true;
+        if (that == null) return false;
+        if (getClass() != that.getClass()) return false;
+        Length length = (Length) that;
         return Double.compare(length.value, value) == 0 && unit == length.unit;
     }
+
 }
--- src/test/java/quantitymeasurement/QuantityTest.java
+++ src/test/java/quantitymeasurement/QuantityTest.java
@@ -9,8 +9,8 @@ public class QuantityTest {
     public void given0FeetAnd0Feet_shouldReturnEqual() {
         Length feet1 = new Length(Length.Unit.FEET,0.0);
         Length feet2 = new Length(Length.Unit.FEET,0.0);
-        boolean equal = feet1.equals(feet2);
-        Assertions.assertTrue(equal);
+        boolean result = feet1.compare(feet2);
+        Assertions.assertTrue(result);
     }
 
     @Test
@@ -18,8 +18,8 @@ public class QuantityTest {
         try {
             Length feet1 = new Length(Length.Unit.FEET,0.0);
             Length feet2 = new Length(Length.Unit.FEET,1.0);
-            boolean equal = feet1.equals(feet2);
-            Assertions.assertFalse(equal);
+            boolean result = feet1.compare(feet2);
+            Assertions.assertFalse(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,7 +30,7 @@ public class QuantityTest {
         try {
             Length feet = null;
             Length feet2 = new Length(Length.Unit.FEET,1.0);
-            boolean result = feet2.equals(feet);
+            boolean result = feet2.compare(feet);
             Assertions.assertFalse(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,7 +41,7 @@ public class QuantityTest {
     public void givenSameFeet_whenCompared_shouldReturnEqual() {
         try {
             Length feet2 = new Length(Length.Unit.FEET,1.0);
-            boolean result = feet2.equals(feet2);
+            boolean result = feet2.compare(feet2);
             Assertions.assertTrue(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,16 +52,16 @@ public class QuantityTest {
     public void given0InchAnd0Inch_shouldReturnEqual() {
         Length inch1 = new Length(Length.Unit.INCH,0.0);
         Length inch2 = new Length(Length.Unit.INCH,0.0);
-        boolean equal = inch1.equals(inch2);
-        Assertions.assertTrue(equal);
+        boolean result = inch1.compare(inch2);
+        Assertions.assertTrue(result);
     }
 
     @Test
     public void given0InchAnd1Inch_shouldReturnNotEqual() {
         Length inch1 = new Length(Length.Unit.INCH,0.0);
         Length inch2 = new Length(Length.Unit.INCH,1.0);
-        boolean equal = inch1.equals(inch2);
-        Assertions.assertFalse(equal);
+        boolean result = inch1.compare(inch2);
+        Assertions.assertFalse(result);
     }
 
     @Test
@@ -69,7 +69,7 @@ public class QuantityTest {
         try {
             Length inch = null;
             Length inch1 = new Length(Length.Unit.INCH,0.0);
-            boolean result = inch1.equals(inch);
+            boolean result = inch1.compare(inch);
             Assertions.assertFalse(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,7 +80,7 @@ public class QuantityTest {
     public void givenSameInch_whenCompared_shouldReturnEqual() {
         try {
             Length inch1 = new Length(Length.Unit.INCH,0.0);
-            boolean result = inch1.equals(inch1);
+            boolean result = inch1.compare(inch1);
             Assertions.assertTrue(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -92,7 +92,7 @@ public class QuantityTest {
         try {
             Length feet = new Length(Length.Unit.FEET,0.0);
             Length inch = new Length(Length.Unit.INCH,0.0);
-            boolean result = feet.equals(inch);
+            boolean result = feet.compare(inch);
             Assertions.assertTrue(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,7 +104,7 @@ public class QuantityTest {
         try {
             Length feet = new Length(Length.Unit.FEET,1.0);
             Length inch = new Length(Length.Unit.INCH,12.0);
-            boolean result = feet.equals(inch);
+            boolean result = feet.compare(inch);
             Assertions.assertTrue(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -116,7 +116,7 @@ public class QuantityTest {
         try {
             Length feet = new Length(Length.Unit.FEET,1.0);
             Length inch = new Length(Length.Unit.INCH,12.0);
-            boolean result = inch.equals(feet);
+            boolean result = inch.compare(feet);
             Assertions.assertTrue(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -128,7 +128,7 @@ public class QuantityTest {
         try {
             Length feet = new Length(Length.Unit.FEET,1.0);
             Length inch = new Length(Length.Unit.INCH,2.0);
-            boolean result = feet.equals(inch);
+            boolean result = feet.compare(inch);
             Assertions.assertFalse(result);
         } catch (Exception e) {
             e.printStackTrace();
@@ -140,10 +140,78 @@ public class QuantityTest {
         try {
             Length inch = new Length(Length.Unit.INCH,2.0);
             Length feet = new Length(Length.Unit.FEET,2.0);
-            boolean result = inch.equals(feet);
+            boolean result = inch.compare(feet);
             Assertions.assertFalse(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void given3FeetAnd1Yard_shouldReturnEqual(){
+        try {
+            Length feet = new Length(Length.Unit.FEET,3.0);
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            boolean result = feet.compare(yard);
+            Assertions.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void given1YardAnd3Feet_shouldReturnEqual(){
+        try {
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            Length feet = new Length(Length.Unit.FEET,3.0);
+            boolean result = yard.compare(feet);
+            Assertions.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void given1FeetAnd1Yard_shouldReturnNotEqual(){
+        try {
+            Length feet = new Length(Length.Unit.FEET,1.0);
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            boolean result = feet.compare(yard);
+            Assertions.assertFalse(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void given1InchAnd1Yard_shouldReturnNotEqual(){
+        try {
+            Length inch = new Length(Length.Unit.INCH,1.0);
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            boolean result = inch.compare(yard);
+            Assertions.assertFalse(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void given36InchAnd1Yard_shouldReturnEqual(){
+        try {
+            Length inch = new Length(Length.Unit.INCH,36.0);
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            boolean result = inch.compare(yard);
+            Assertions.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void given1YardAnd36Inch_shouldReturnEqual(){
+        try {
+            Length yard = new Length(Length.Unit.YARD,1.0);
+            Length inch = new Length(Length.Unit.INCH,36.0);
+            boolean result = yard.compare(inch);
+            Assertions.assertTrue(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
